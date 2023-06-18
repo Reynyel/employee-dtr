193302f49378f61418cefa3e81e4862b973a3488
Ranielle <raniellecruz65@gmail.com>
2023-06-18 20:56:02 +0800

    added implementation to build.gradle. also declared the scan method and a global variable

--- app/src/main/java/com/example/employee_dtr/homepage.java
+++ app/src/main/java/com/example/employee_dtr/homepage.java
@@ -3,12 +3,26 @@ package com.example.employee_dtr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class homepage extends AppCompatActivity {
 
+    ImageButton tin_scan; //global variable for timein scan
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        tin_scan=findViewById(R.id.timeinBtn);
+
+        tin_scan.setOnClickListener(view -> {
+            scanCode();
+        });
+
+    }
+
+    private void scanCode(){
+        
     }
 }
