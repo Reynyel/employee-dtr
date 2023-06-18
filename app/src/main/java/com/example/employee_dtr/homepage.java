faf327429ff1cad2012fc1b9f41690283afcf927
Ranielle <raniellecruz65@gmail.com>
2023-06-18 21:13:22 +0800

    declared captureact activity in AndroidManifest.xml also set permission to camera declared activityresultlauncer

--- app/src/main/java/com/example/employee_dtr/homepage.java
+++ app/src/main/java/com/example/employee_dtr/homepage.java
@@ -1,11 +1,16 @@
 package com.example.employee_dtr;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.zxing.client.android.Intents;
+import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
 public class homepage extends AppCompatActivity {
@@ -30,5 +35,20 @@ public class homepage extends AppCompatActivity {
         options.setBeepEnabled(true);
         options.setOrientationLocked(true);
         options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+
     }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if(result.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(homepage.this);
+            builder.setTitle("Time in");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
 }
